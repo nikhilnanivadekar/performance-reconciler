3ef77469d5eb3cd5ff96261203ded08c7192396c
Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>
2021-05-23 12:19:42 -0500

    add ignoreDuplicates parameter
    Signed-off-by: Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>

--- common/src/main/java/nikhil/nani/data/bean/ReconcilerRequest.java
+++ common/src/main/java/nikhil/nani/data/bean/ReconcilerRequest.java
@@ -6,17 +6,19 @@ public class ReconcilerRequest
     private String pathFile2;
     private RequestType requestType;
     private ReconcilerModuleType reconcilerModuleType;
+    private boolean ignoreDuplicates;
 
     public ReconcilerRequest()
     {
     }
 
-    public ReconcilerRequest(String pathFile1, String pathFile2, RequestType requestType, ReconcilerModuleType reconcilerModuleType)
+    public ReconcilerRequest(String pathFile1, String pathFile2, RequestType requestType, ReconcilerModuleType reconcilerModuleType, boolean ignoreDuplicates)
     {
         this.pathFile1 = pathFile1;
         this.pathFile2 = pathFile2;
         this.requestType = requestType;
         this.reconcilerModuleType = reconcilerModuleType;
+        this.ignoreDuplicates = ignoreDuplicates;
     }
 
     public String getPathFile1()
@@ -59,6 +61,16 @@ public class ReconcilerRequest
         this.reconcilerModuleType = reconcilerModuleType;
     }
 
+    public boolean isIgnoreDuplicates()
+    {
+        return this.ignoreDuplicates;
+    }
+
+    public void setIgnoreDuplicates(boolean ignoreDuplicates)
+    {
+        this.ignoreDuplicates = ignoreDuplicates;
+    }
+
     @Override
     public boolean equals(Object o)
     {
@@ -73,6 +85,10 @@ public class ReconcilerRequest
 
         ReconcilerRequest that = (ReconcilerRequest) o;
 
+        if (this.ignoreDuplicates != that.ignoreDuplicates)
+        {
+            return false;
+        }
         if (this.pathFile1 != null ? !this.pathFile1.equals(that.pathFile1) : that.pathFile1 != null)
         {
             return false;
@@ -95,6 +111,7 @@ public class ReconcilerRequest
         result = 31 * result + (this.pathFile2 != null ? this.pathFile2.hashCode() : 0);
         result = 31 * result + (this.requestType != null ? this.requestType.hashCode() : 0);
         result = 31 * result + (this.reconcilerModuleType != null ? this.reconcilerModuleType.hashCode() : 0);
+        result = 31 * result + (this.ignoreDuplicates ? 1 : 0);
         return result;
     }
 }

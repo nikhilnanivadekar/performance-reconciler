2d7db1f6ec53849c80ce2680b4350db59cfd2c19
Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>
2021-05-22 20:49:18 -0500

    Add ReconcilerRequest
    Signed-off-by: Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>

---
+++ common/src/main/java/nikhil/nani/data/bean/ReconcilerRequest.java
@@ -0,0 +1,83 @@
+package nikhil.nani.data.bean;
+
+public class ReconcilerRequest
+{
+    private String pathFile1;
+    private String pathFile2;
+    private RequestType requestType;
+
+    public ReconcilerRequest()
+    {
+    }
+
+    public ReconcilerRequest(String pathFile1, String pathFile2, RequestType requestType)
+    {
+        this.pathFile1 = pathFile1;
+        this.pathFile2 = pathFile2;
+        this.requestType = requestType;
+    }
+
+    public String getPathFile1()
+    {
+        return this.pathFile1;
+    }
+
+    public void setPathFile1(String pathFile1)
+    {
+        this.pathFile1 = pathFile1;
+    }
+
+    public String getPathFile2()
+    {
+        return this.pathFile2;
+    }
+
+    public void setPathFile2(String pathFile2)
+    {
+        this.pathFile2 = pathFile2;
+    }
+
+    public RequestType getRequestType()
+    {
+        return this.requestType;
+    }
+
+    public void setRequestType(RequestType requestType)
+    {
+        this.requestType = requestType;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass())
+        {
+            return false;
+        }
+
+        ReconcilerRequest that = (ReconcilerRequest) o;
+
+        if (this.pathFile1 != null ? !this.pathFile1.equals(that.pathFile1) : that.pathFile1 != null)
+        {
+            return false;
+        }
+        if (this.pathFile2 != null ? !this.pathFile2.equals(that.pathFile2) : that.pathFile2 != null)
+        {
+            return false;
+        }
+        return this.requestType == that.requestType;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = this.pathFile1 != null ? this.pathFile1.hashCode() : 0;
+        result = 31 * result + (this.pathFile2 != null ? this.pathFile2.hashCode() : 0);
+        result = 31 * result + (this.requestType != null ? this.requestType.hashCode() : 0);
+        return result;
+    }
+}

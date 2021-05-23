690c7aaed9a4bf0909405278e6e39145599a9d80
Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>
2021-05-22 21:04:46 -0500

    set path to be jdk specific
    Signed-off-by: Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>

--- jdk-reconciler/src/main/java/nikhil/nani/reconciler/jdk/controller/JdkReconcilerController.java
+++ jdk-reconciler/src/main/java/nikhil/nani/reconciler/jdk/controller/JdkReconcilerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/reconcile")
+@RequestMapping("/jdk-reconcile")
 public class JdkReconcilerController
 {
     private ReconcilerService reconcilerService;

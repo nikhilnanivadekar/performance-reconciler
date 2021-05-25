70b3f5cc5e7d4e471944ac02dec15583d47588bf
Donald Raab <Donald.Raab@bnymellon.com>
2021-05-25 14:11:27 -0400

    Add flip to MultimapTest.

--- examples/src/test/java/datastructures/multimap/MultimapTest.java
+++ examples/src/test/java/datastructures/multimap/MultimapTest.java
@@ -1,5 +1,6 @@
 package datastructures.multimap;
 
+import org.eclipse.collections.api.factory.Bags;
 import org.eclipse.collections.api.factory.Lists;
 import org.eclipse.collections.api.multimap.Multimap;
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +11,7 @@ public class MultimapTest
     private final String words = "The quick brown fox jumps over the lazy dog";
 
     @Test
-    void groupWordsByFirstCharacter()
+    public void groupWordsByFirstCharacter()
     {
         Multimap<Character, String> multimap =
                 Lists.mutable.with(this.words.split(" "))
@@ -19,5 +20,18 @@ public class MultimapTest
 
         Assertions.assertEquals(Lists.mutable.with("the", "the"), multimap.get('t'));
         Assertions.assertEquals(Lists.mutable.empty(), multimap.get('a'));
+    }
+
+    @Test
+    public void flip()
+    {
+        Multimap<String, Character> multimap =
+                Lists.mutable.with(this.words.split(" "))
+                        .collect(String::toLowerCase)
+                        .groupBy(each -> each.charAt(0))
+                        .flip();
+
+        Assertions.assertEquals(Bags.mutable.with('t', 't'), multimap.get("the"));
+        Assertions.assertEquals(Bags.mutable.empty(), multimap.get("and"));
     }
 }

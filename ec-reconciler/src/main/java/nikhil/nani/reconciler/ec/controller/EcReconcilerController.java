ee31bdf3864b99e17ff3019f22289a95a6885bbd
Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>
2021-06-06 10:07:57 -0500

    Wire different EC reconciler services
    Signed-off-by: Nikhil Nanivadekar <nikhil.nanivadekar@bnymellon.com>

--- ec-reconciler/src/main/java/nikhil/nani/reconciler/ec/controller/EcReconcilerController.java
+++ ec-reconciler/src/main/java/nikhil/nani/reconciler/ec/controller/EcReconcilerController.java
@@ -1,7 +1,10 @@
 package nikhil.nani.reconciler.ec.controller;
 
 import nikhil.nani.data.bean.ReconcilerRequest;
-import nikhil.nani.data.service.ReconcilerService;
+import nikhil.nani.reconciler.ec.service.impl.EcForEachInBothReconcilerServiceImpl;
+import nikhil.nani.reconciler.ec.service.impl.EcReconcilerServiceImpl;
+import nikhil.nani.reconciler.ec.service.impl.EcZipReconcilerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,16 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ec-reconcile")
 public class EcReconcilerController
 {
-    private ReconcilerService reconcilerService;
+    private final EcForEachInBothReconcilerServiceImpl ecForEachInBothReconcilerServiceImpl;
+    private final EcReconcilerServiceImpl ecReconcilerServiceImpl;
+    private final EcZipReconcilerServiceImpl ecZipReconcilerServiceImpl;
 
-    public EcReconcilerController(ReconcilerService reconcilerService)
+    @Autowired
+    public EcReconcilerController(
+            EcForEachInBothReconcilerServiceImpl ecForEachInBothReconcilerServiceImpl,
+            EcReconcilerServiceImpl ecReconcilerServiceImpl,
+            EcZipReconcilerServiceImpl ecZipReconcilerServiceImpl)
     {
-        this.reconcilerService = reconcilerService;
+        this.ecForEachInBothReconcilerServiceImpl = ecForEachInBothReconcilerServiceImpl;
+        this.ecReconcilerServiceImpl = ecReconcilerServiceImpl;
+        this.ecZipReconcilerServiceImpl = ecZipReconcilerServiceImpl;
     }
 
-    @PostMapping("/predefined-objects")
-    public ResponseEntity<String> reconcile(@RequestBody ReconcilerRequest request)
+    @PostMapping("/ecForEachInBothReconcilerServiceImpl")
+    public ResponseEntity<String> ecForEachInBothReconcilerServiceImpl(@RequestBody ReconcilerRequest request)
     {
-        return ResponseEntity.ok(this.reconcilerService.reconcile(request));
+        return ResponseEntity.ok(this.ecForEachInBothReconcilerServiceImpl.reconcile(request));
+    }
+
+    @PostMapping("/ecReconcilerServiceImpl")
+    public ResponseEntity<String> ecReconcilerServiceImpl(@RequestBody ReconcilerRequest request)
+    {
+        return ResponseEntity.ok(this.ecReconcilerServiceImpl.reconcile(request));
+    }
+
+    @PostMapping("/ecZipReconcilerServiceImpl")
+    public ResponseEntity<String> ecZipReconcilerServiceImpl(@RequestBody ReconcilerRequest request)
+    {
+        return ResponseEntity.ok(this.ecZipReconcilerServiceImpl.reconcile(request));
     }
 }
--- ec-reconciler/src/test/java/nikhil/nani/reconciler/ec/controller/EcReconcilerControllerTest.java
+++ ec-reconciler/src/test/java/nikhil/nani/reconciler/ec/controller/EcReconcilerControllerTest.java
@@ -1,7 +1,9 @@
 package nikhil.nani.reconciler.ec.controller;
 
 import nikhil.nani.data.bean.ReconcilerRequest;
-import nikhil.nani.data.service.ReconcilerService;
+import nikhil.nani.reconciler.ec.service.impl.EcForEachInBothReconcilerServiceImpl;
+import nikhil.nani.reconciler.ec.service.impl.EcReconcilerServiceImpl;
+import nikhil.nani.reconciler.ec.service.impl.EcZipReconcilerServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,17 +19,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EcReconcilerControllerTest
 {
     @Mock
-    private ReconcilerService service;
+    private EcForEachInBothReconcilerServiceImpl ecForEachInBothReconcilerServiceImpl;
+    @Mock
+    private EcReconcilerServiceImpl ecReconcilerServiceImpl;
+    @Mock
+    private EcZipReconcilerServiceImpl ecZipReconcilerServiceImpl;
 
     @InjectMocks
     private EcReconcilerController testObj;
 
     @Test
-    public void reconcile()
+    public void ecForEachInBothReconcilerServiceImpl()
     {
-        Mockito.when(this.service.reconcile(new ReconcilerRequest())).thenReturn("reconciled");
+        Mockito.when(this.ecForEachInBothReconcilerServiceImpl.reconcile(new ReconcilerRequest()))
+                .thenReturn("reconciled");
 
-        ResponseEntity<String> response = this.testObj.reconcile(new ReconcilerRequest());
+        ResponseEntity<String> response = this.testObj.ecForEachInBothReconcilerServiceImpl(new ReconcilerRequest());
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("reconciled", response.getBody());
+    }
+
+    @Test
+    public void ecZipReconcilerServiceImpl()
+    {
+        Mockito.when(this.ecZipReconcilerServiceImpl.reconcile(new ReconcilerRequest()))
+                .thenReturn("reconciled");
+
+        ResponseEntity<String> response = this.testObj.ecZipReconcilerServiceImpl(new ReconcilerRequest());
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("reconciled", response.getBody());
+    }
+
+    @Test
+    public void ecReconcilerServiceImpl()
+    {
+        Mockito.when(this.ecReconcilerServiceImpl.reconcile(new ReconcilerRequest()))
+                .thenReturn("reconciled");
+
+        ResponseEntity<String> response = this.testObj.ecReconcilerServiceImpl(new ReconcilerRequest());
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("reconciled", response.getBody());
